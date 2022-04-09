5f5d26e197a807541a4448a2a969d79d1ea8f686
Makii117 <MakiOnPc@gmail.com>
2022-04-09 18:27:33 +0200

    Added semaphore poc

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,12 @@ import React from "react";
 import { MantineProvider } from "@mantine/core";
 import { Navigation } from "./components/navbar/Navigation";
 import Home from "./pages/Home";
-
+import {TestingPage} from "./pages/testingPage";
 function App() {
   return (
-    <MantineProvider
-      theme={{
-        breakpoints: {
-          xs: 500,
-          sm: 800,
-          md: 1000,
-          lg: 1200,
-          xl: 1400,
-        },
-        colorScheme: "light",
-        //  //  //  //
-        colors: {
-          blue: ["#3d5a80", "#98c1d9", "#e0fbfc", "#ee6c4d", "#293241"],
-        },
-        headings: {
-          fontFamily: "Roboto, sans-serif",
-          sizes: {
-            h1: { fontSize: 40 },
-          },
-          textDecoration: "none",
-        },
-      }}
-    >
-      <div>
-        <Navigation />
-        <div>
-          <Home />
-        </div>
-      </div>
-    </MantineProvider>
+   <div>
+     <TestingPage></TestingPage>
+   </div>
   );
 }
 
--- src/components/trafficNotifications/trafficNotification.tsx
+++ src/components/trafficNotifications/trafficNotification.tsx
@@ -1,1 +1,13 @@
-export {}
+import { Select, Button, Box, Container, Text} from '@mantine/core';
+import { useState } from 'react';
+import './trafficNotification.css';
+
+export function TrafficNotification() {
+    const [value,setValue]=useState('')
+    
+    return (
+        <Container className="testContainer">
+                <h1>Test</h1>
+        </Container>
+    );
+}
