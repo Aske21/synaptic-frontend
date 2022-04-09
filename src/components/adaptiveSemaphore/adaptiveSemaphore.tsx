c5a483a1b17f2bbeb187775ea1b6e848b49a3d27
Makii117 <MakiOnPc@gmail.com>
2022-04-09 18:43:16 +0100

    Added traffic notification poc

--- src/components/adaptiveSemaphore/adaptiveSemaphore.tsx
+++ src/components/adaptiveSemaphore/adaptiveSemaphore.tsx
@@ -1,4 +1,5 @@
 import { Select, Button, Box, Container, Text } from "@mantine/core";
+import { wrap } from "module";
 import { useState } from "react";
 
 import "./adaptiveSemaphore.css";
@@ -7,9 +8,13 @@ export function AdaptiveSemaphore() {
   const [value, setValue] = useState("");
   const [congestion1, setCongestion1] = useState(0.1);
   const [congestion2, setCongestion2] = useState(0.7);
+  const [light1, setLight1] = useState("Green");
+  const [light2, setLight2] = useState("Red");
+  const [crossroads, setCrossroads] = useState("Raskrsnica Pofalici");
   async function adapt() {
     var c1 = congestion1;
     var c2 = congestion2;
+    var i = 0;
     do {
       c1 = strip(c1);
       c2 = strip(c2);
@@ -17,21 +22,33 @@ export function AdaptiveSemaphore() {
       setCongestion1(c1);
       setCongestion2(c2);
       if (c1 < c2) {
+        setLight2("Green");
+        setLight1("Red");
+        await sleep(getRandomInt(1, 2) * 1000);
         c1 += 0.1;
         await sleep(getRandomInt(1, 2) * 1000);
         c2 -= 0.1;
         c1 = strip(c1);
         c2 = strip(c2);
-      } else if (c2 > c1) {
+      } else if (c2 < c1) {
+        setLight2("Red");
+        setLight1("Green");
+        await sleep(getRandomInt(1, 2) * 1000);
         c1 -= 0.1;
         await sleep(getRandomInt(1, 2) * 1000);
         c2 += 0.1;
         c1 = strip(c1);
         c2 = strip(c2);
+      } else if (c2 == c1) {
+        await sleep(getRandomInt(1, 2) * 1000);
+        c1 += 0.2;
+        await sleep(getRandomInt(1, 2) * 1000);
+        c2 -= 0.2;
       }
       setCongestion1(c1);
       setCongestion2(c2);
-    } while (c1 !== c2);
+      i += 1;
+    } while (i < 3);
   }
   function strip(c: number) {
     return parseFloat(c.toPrecision(1));
@@ -47,10 +64,29 @@ export function AdaptiveSemaphore() {
   }
   return (
     <Container className="testContainer">
-      <Text className="headerTitle">Adaptive semaphore</Text>
-      <Box className="testBox"></Box>
-      <Text id="congestion1">S1 congestion: {congestion1}</Text>
-      <Text id="congestion2">S2 congestion: {congestion2}</Text>
+      <Text className="headerTitle" style={{ marginBottom: 3 }}>
+        Adaptive semaphore
+      </Text>
+      <Text
+        style={{
+          marginBottom: 10,
+          marginTop: 7,
+          fontSize: "1rem",
+          fontWeight: 700,
+          border: "2px solid",
+          width: "50%",
+        }}
+      >
+        {crossroads}
+      </Text>
+      <Box className="semaphoreBox">
+        <Text id="s1">S1</Text>
+        <Text id="s2">S2</Text>
+        <Text id="l1">Light: {light1}</Text>
+        <Text id="l2">Light: {light2}</Text>
+        <Text id="c1">Congestion: {congestion1}</Text>
+        <Text id="c2">Congestion: {congestion2}</Text>
+      </Box>
       <Box>
         <Button onClick={adapt} className="button">
           Adapt Semaphores
--- src/components/trafficNotifications/trafficNotification.tsx
+++ src/components/trafficNotifications/trafficNotification.tsx
@@ -1,13 +1,38 @@
-import { Select, Button, Box, Container, Text} from '@mantine/core';
-import { useState } from 'react';
-import './trafficNotification.css';
+import { Select, Button, Box, Container, Text } from "@mantine/core";
+import { useEffect, useState } from "react";
+import "./trafficNotification.css";
 
 export function TrafficNotification() {
-    const [value,setValue]=useState('')
-    
-    return (
-        <Container className="testContainer">
-                <h1>Test</h1>
-        </Container>
-    );
+  const [warn, setWarn] = useState("");
+  function generateTrafficNotification() {
+    var trafficWarnings = [
+      "Sudar u nedzaricima prema Ilidzi",
+      "Tramvaj izletio sa sina kod muzeja",
+      "Poplavljen podvoznja u buca potoku",
+      "Zaledila cesta prema vratniku",
+    ];
+    var randomNotification =
+      trafficWarnings[Math.floor(Math.random() * trafficWarnings.length)]; //pl
+
+    setWarn(randomNotification);
+    setTimeout(function () {
+      alert(randomNotification);
+    }, 50);
+  }
+  return (
+    <Container className="testContainer">
+      <Text className="headerTitle">Traffic Notification</Text>
+      <Box className="testBox">
+        <Button
+          onClick={() => generateTrafficNotification()}
+          className="button"
+        >
+          Generate Traffic Notification
+        </Button>
+        <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "bold" }}>
+          {warn}
+        </Text>
+      </Box>
+    </Container>
+  );
 }
